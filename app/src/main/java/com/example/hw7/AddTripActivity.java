20f35ce90abb05686fda53fac55879ed07f886ac
Nia Ibrahim <nibrahi3@uncc.edu>
2019-11-20 22:25:20 -0500

    Successfully added google map fragment

--- app/src/main/java/com/example/hw7/AddTripActivity.java
+++ app/src/main/java/com/example/hw7/AddTripActivity.java
@@ -2,16 +2,18 @@ package com.example.hw7;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,61 +25,114 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class AddTripActivity extends AppCompatActivity  {
+public class AddTripActivity extends AppCompatActivity {
 
-    public EditText et_title;
-    public EditText et_description;
-    public Button btn_submit;
-    // Access a Cloud Firestore instance from your Activity
-    public FirebaseFirestore db = FirebaseFirestore.getInstance();
-    public FirebaseAuth mAuth;
-    public String userId;
-    public String tripId;
+//    public EditText et_title;
+//    public EditText et_description;
+//    public Button btn_submit;
+//    // Access a Cloud Firestore instance from your Activity
+//    public FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    public FirebaseAuth mAuth;
+//    public String userId;
+//    public String tripId;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_add_trip);
+
+//
+////        et_title = findViewById(R.id.et_title);
+////        et_description = findViewById(R.id.et_descrption);
+////        btn_submit = findViewById(R.id.btn_submit);
+//
+//        // Initialize Firebase Auth
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        userId = user.getUid();
+//
+//        tripId = UUID.randomUUID().toString();
+//
+//
+////        btn_submit.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Map<String, Object> tripMap = new HashMap<>();
+////                tripMap.put("userId", userId);
+////                tripMap.put("tripId", tripId);
+////                tripMap.put("title", et_title.getText().toString());
+////                tripMap.put("description", et_description.getText().toString());
+////
+////                db.collection("trips").document(tripId)
+////                        .set(tripMap)
+////                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+////                            @Override
+////                            public void onSuccess(Void aVoid) {
+////
+////                            }
+////                        })
+////                        .addOnFailureListener(new OnFailureListener() {
+////                            @Override
+////                            public void onFailure(@NonNull Exception e) {
+////
+////                            }
+////                        });
+//            //}
+//        //});
+//
+////        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+////                .findFragmentById(R.id.map);
+////        mapFragment.getMapAsync((OnMapReadyCallback) this);
+//
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//
+//    }
+//
+//    @Override
+//    public void onPointerCaptureChanged(boolean hasCapture) {
+//
+//    }
+
+
+    public Button btn1;
+    public Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trip);
         setTitle("Add Trip");
-
-        et_title = findViewById(R.id.et_title);
-        et_description = findViewById(R.id.et_descrption);
-        btn_submit = findViewById(R.id.btn_submit);
-
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-
-        FirebaseUser user = mAuth.getCurrentUser();
-        userId = user.getUid();
-
-        tripId = UUID.randomUUID().toString();
-
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Map<String, Object> tripMap = new HashMap<>();
-                tripMap.put("userId", userId);
-                tripMap.put("tripId", tripId);
-                tripMap.put("title", et_title.getText().toString());
-                tripMap.put("description", et_description.getText().toString());
-
-                db.collection("trips").document(tripId)
-                        .set(tripMap)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-
-                            }
-                        });
-            }
-        });
+//        btn1 = findViewById(R.id.btn1);
+//        btn2 = findViewById(R.id.btn2);
+//
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                addFragment(new OneFragment(), false, "one");
+//            }
+//        });
+//
+//        btn2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+                addFragment(new MapFragment(), false, "one");
+//            }
+//        });
 
     }
 
+    public void addFragment(Fragment fragment, boolean addToBackStack, String tag) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if (addToBackStack) {
+            ft.addToBackStack(tag);
+        }
+        ft.replace(R.id.container_frame_back, fragment, tag);
+        ft.commitAllowingStateLoss();
+    }
 }
--- app/src/main/java/com/example/hw7/MapFragment.java
+++ app/src/main/java/com/example/hw7/MapFragment.java
@@ -1,49 +1,96 @@
 package com.example.hw7;
 
+
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment  implements OnMapReadyCallback {
 
-    SupportMapFragment supportMapFragment;
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapFragment extends Fragment {
+
     public MapFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.fragment_map, container, false);
-        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if(supportMapFragment == null){
-            FragmentManager fm  = getFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            supportMapFragment = SupportMapFragment.newInstance();
-            ft.replace(R.id.map, supportMapFragment).commit();
-        }
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
-        supportMapFragment.getMapAsync(this);
-        return v;
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mMap.clear(); //clear old markers
+
+                CameraPosition googlePlex = CameraPosition.builder()
+                        .target(new LatLng(37.4219999,-122.0862462))
+                        .zoom(10)
+                        .bearing(0)
+                        .tilt(45)
+                        .build();
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4219999, -122.0862462))
+                        .title("Spider Man")
+                        .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.common_google_signin_btn_icon_dark)));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.4629101,-122.2449094))
+                        .title("Iron Man")
+                        .snippet("His Talent : Plenty of money"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(37.3092293,-122.1136845))
+                        .title("Captain America"));
+            }
+        });
+
+
+        return rootView;
     }
 
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
+
 
 }
